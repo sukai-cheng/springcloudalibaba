1cc0b668429e95dc105571ca63fd32d682206665
chengsukai <1710290870@qq.com>
2022-09-08 17:16:22 +0800

    init：创建微服务项目

--- stock/src/main/java/com/tulingxueyuan/stock/StockApplication.java
+++ stock/src/main/java/com/tulingxueyuan/stock/StockApplication.java
@@ -1,6 +1,15 @@
-package com.tulingxueyuan.stock.controller.xxx;/**
-* @author chengsukai
-* @since 2022-09-08 16:31
-*/
+package com.tulingxueyuan.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chengsukai
+ * @since 2022-09-08 16:31
+ */
+@SpringBootApplication
 public class StockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockApplication.class, args);
+    }
 }
--- stock/src/main/java/com/tulingxueyuan/stock/controller/StockController.java
+++ stock/src/main/java/com/tulingxueyuan/stock/controller/StockController.java
@@ -1,6 +1,19 @@
-package com.tulingxueyuan.stock.controller;/**
-* @author chengsukai
-* @since 2022-09-08 16:25
-*/
+package com.tulingxueyuan.stock.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chengsukai
+ * @since 2022-09-08 16:25
+ */
+@RestController
+@RequestMapping("/stock")
 public class StockController {
+
+    @RequestMapping("reduce")
+    public String reduct() {
+        System.out.println("扣减库存");
+        return "扣减库存";
+    }
 }
