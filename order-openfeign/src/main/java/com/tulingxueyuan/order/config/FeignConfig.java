9b5c66b07325f79685d25082d178f154691ec356
chengsukai <1710290870@qq.com>
2022-09-13 14:27:38 +0800

    add: 添加feign全局配置

---
+++ order-openfeign/src/main/java/com/tulingxueyuan/order/config/FeignConfig.java
@@ -0,0 +1,18 @@
+package com.tulingxueyuan.order.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author chengsukai
+ * @since 2022-09-13 14:25
+ */
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+}
