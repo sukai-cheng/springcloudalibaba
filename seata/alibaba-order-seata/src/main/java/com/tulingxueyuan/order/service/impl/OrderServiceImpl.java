4e926c9e5808b78ac4de227885c6cc736800fc30
chengsukai <1710290870@qq.com>
2022-09-15 16:20:59 +0800

    fix: seata使用

--- seata/alibaba-order-seata/src/main/java/com/tulingxueyuan/order/service/impl/OrderServiceImpl.java
+++ seata/alibaba-order-seata/src/main/java/com/tulingxueyuan/order/service/impl/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import com.tulingxueyuan.order.feign.StockFeignService;
 import com.tulingxueyuan.order.mapper.OrderTblMapper;
 import com.tulingxueyuan.order.pojo.OrderTbl;
 import com.tulingxueyuan.order.service.OrderService;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,14 +27,11 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     StockFeignService stockFeignService;
 
-    @Transactional
+    @GlobalTransactional
     @Override
     public void create(OrderTbl order) {
 
         orderTblMapper.insert(order);
         String msg = stockFeignService.reduct(order.getId());
-
-        int a = 1 / 0;
-
     }
 }
